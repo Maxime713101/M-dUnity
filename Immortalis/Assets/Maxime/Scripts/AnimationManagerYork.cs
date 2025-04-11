a9ecbebf7a98f3200af9037c0b52390b0d182510
Maxime713101 <148874548+Maxime713101@users.noreply.github.com>
2025-04-11 15:56:57 +0200

    Début de code pour transition d'animation Mr.York

--- Immortalis/Assets/Maxime/Scripts/AnimationManagerYork.cs
+++ Immortalis/Assets/Maxime/Scripts/AnimationManagerYork.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class TransitionAnimationDiscussion : MonoBehaviour
 {
-    public Animator animator;
+    public Animator animatorYork;
     public GameObject Player;
+    private bool IsTalking;
     private bool Islookingplayer;
     private Vector3 CustomLookAt;
+    public GameObject StopPosition;
+    private float speed = 1.0f;
+    private bool IsMoving;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,15 +34,36 @@ public class TransitionAnimationDiscussion : MonoBehaviour
             
             Debug.DrawRay(transform.position, transform.forward,Color.green);
         }
+
+        if (IsMoving == true) 
+        { 
+            if (transform.position.z >= StopPosition.transform.position.z)
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            else
+            {
+                //Mettre l'animation de parlote (faire une transition dans l'animator)
+            }
+            
+        }
+
+        
     }
     IEnumerator Findelaconversation()
     {
         yield return new WaitForSeconds(2);
-        animator.SetBool("Transition", true);
+        animatorYork.SetBool("Transition", true);
         Islookingplayer = true;
+        IsMoving = true;
     }
     public void findelaconversation() 
     {
         StartCoroutine(Findelaconversation());
+        
+    }
+    public void debutconversation()
+    {
+        animatorYork.SetBool("IsTalking", true);
     }
 }
